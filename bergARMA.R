4fe58410d2ea80b18f5272ad16247c137b45064b
Lucas Sales <49413205+lucasdofs@users.noreply.github.com>
2021-05-22 14:40:07 -0300

    Update bergARMA.R

--- bergARMA.R
+++ bergARMA.R
@@ -1,4 +1,4 @@
-bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
+bergARMA<- function(beta,phi,theta,nu, y, X, link = "log"){
   require(bergrm)
   #Link function: Useful for the conditional Log-Likelihood function, Conditional score and Jacobian matrix
   g <- function(link){
@@ -38,13 +38,13 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
                    deriv.. = deriv.., valideta = valideta,
                    name = link), class = "link-sdlrm")
   }
-
+  
   ##### ARMA model 
   if(phi != 0 && theta != 0){
     print("ARMA model")
     
     #Necessary quantities for the CLL function, score and jacobian functions
-    par<- c(alpha,beta,phi,theta,nu)  
+    par<- c(beta,phi,theta,nu)  
     n=length(y)
     p=length(phi);q=length(theta)
     m= max(p,q)
@@ -63,15 +63,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       eta=error=rep(0,length(y))
       m=max(p,q)
       
-      alpha <- par[1]
-      beta<- par[2:(ncol(X)+1)]
-      phi<-par[(ncol(X)+2):(ncol(X)+p+1)]
-      theta<-par[(ncol(X)+p+2):(ncol(X)+p+q+1)]
+      beta<- par[1:(ncol(X))]
+      phi<-par[(ncol(X)+1):(ncol(X)+p)]
+      theta<-par[(ncol(X)+p+1):(ncol(X)+p+q)]
       nu<-par[length(par)]
       #Creating the mu_t; obs: here, I put an threshold when ynew = 0 
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) + (theta%*%error[i-(1:q)])
+        eta[i] <- X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) + (theta%*%error[i-(1:q)])
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -101,16 +100,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       eta=error=rep(0,length(y))
       m=max(p,q)
       
-      alpha <- par[1]
-      beta<- par[2:(ncol(X)+1)]
-      phi<-par[(ncol(X)+2):(ncol(X)+p+1)]
-      theta<-par[(ncol(X)+p+2):(ncol(X)+p+q+1)]
+      beta<- par[1:(ncol(X))]
+      phi<-par[(ncol(X)+1):(ncol(X)+p)]
+      theta<-par[(ncol(X)+p+1):(ncol(X)+p+q)]
       nu<-par[length(par)]
       delta <- (as.numeric(y==0))
-      
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) + (theta%*%error[i-(1:q)])
+        eta[i] <- X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) + (theta%*%error[i-(1:q)])
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -142,14 +139,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       
       
       ###Initializing the derivates in zero
-      deta.dalpha <- matrix(0, ncol=1,nrow=n)
+      #deta.dalpha <- matrix(0, ncol=1,nrow=n)
       deta.dbeta <- matrix(0, ncol=length(beta),nrow=n)
       deta.dphi <- matrix(0, ncol=p,nrow=n)
       deta.dtheta <- matrix(0, ncol=q,nrow=n)
       
       for(i in (m+1):n)
       {
-        deta.dalpha[i,] <- 1 -  theta%*%deta.dalpha[i-(1:q),]
+        #deta.dalpha[i,] <- 1 -  theta%*%deta.dalpha[i-(1:q),]
         deta.dbeta[i,]<- B_aux[(i-m),] - theta%*%deta.dbeta[i-(1:q),]
         deta.dphi[i,] <- P_aux[(i-m),] - theta%*%deta.dphi[i-(1:q),] 
         deta.dtheta[i,]<- T_aux[(i-m),] - theta%*%deta.dtheta[i-(1:q),]
@@ -157,20 +154,20 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       ### Derivate with relation to nu
       deta.dnu <- 2*delta*mu/((1-mu+nu)*(1+mu+nu)) + (1-delta)*( 2*(y-mu-nu)/(mu+nu-1)*(mu+nu+1))
       #Matrices use in the score vector (matrix Q = matrix T of the paper)
-      A <- deta.dalpha[(m+1):n]
+      #A <- deta.dalpha[(m+1):n]
       B <- deta.dbeta[(m+1):n,]
       P <- deta.dphi[(m+1):n,]
       Q <- deta.dtheta[(m+1):n,]
       D <- diag(as.numeric(1/g1.(mu[(m+1):n])))
       V <- V_aux[(m+1):n]
       
-      Ua <- t(A)%*%D%*%V
+      #Ua <- t(A)%*%D%*%V
       Ub <- t(B)%*%D%*%V
       Up <- t(P)%*%D%*%V
       Ut <- t(Q)%*%D%*%V
       Un <- sum(deta.dnu[(m+1): n])
       
-      U<- c(Ua,Ub,Up,Ut,Un)   
+      U<- c(Ub,Up,Ut,Un)   
       return(U)
     }
     
@@ -178,15 +175,15 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
     ll <- function(par) -cond_ll_berg(par, y, X,n,p,q, link = "log")
     U <- function(par) -cond_U_berg(par,y,X,n,p,q,link="log")
     
-    opt <- nlminb(par, ll, gradient = NULL, hessian = NULL, scale = 1, control = list(),
-                  lower = rep(0.0001, ncol(X)+p+q+2), 
-                  upper = c(rep(Inf,ncol(X)+1),rep(1,p+q),Inf))
+    opt <- nlminb(par, ll, gradient = U, hessian = NULL, scale = 1, control = list(),
+                  lower = rep(0.0001, ncol(X)+p+q+1), 
+                  upper = c(rep(Inf,ncol(X)),rep(1,p+q),Inf))
     
     
     aux <- c()
     aux$serie <- y
     aux$par <- opt$par
-    names(aux$par)<-c("alpha",paste("beta",1:ncol(X)),paste("phi",1:p),paste("theta",1:q),"nu")
+    names(aux$par)<-c(paste("beta",0:(ncol(X)-1)),paste("phi",1:p),paste("theta",1:q),"nu")
     aux$loglik <- opt$objective  
     aux$AIC <- 2*aux$loglik+2*(ncol(X)+p+q+1)
     aux$BIC <- 2*aux$loglik+log(n)*(ncol(X)+p+q+1)
@@ -198,17 +195,16 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       # Creating the link functions
       g1 <- g(link)$fun
       g1.inv <- g(link)$inv
-      alpha.hat <- par[1]
-      beta.hat<- par[2:(ncol(X)+1)]
-      phi.hat<-par[(ncol(X)+2):(ncol(X)+p+1)]
-      theta.hat<-par[(ncol(X)+p+2):(ncol(X)+p+q+1)]
+      beta.hat<- par[1:(ncol(X))]
+      phi.hat<-par[(ncol(X)+1):(ncol(X)+p)]
+      theta.hat<-par[(ncol(X)+p+1):(ncol(X)+p+q)]
       nu.hat<-par[length(aux$par)]
       X <- as.matrix(X)
       ynew <- g1.inv(y); 
       eta<-error<-rep(0,length(y))
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) + (theta%*%error[i-(1:q)])
+        eta[i] <- X[i,]%*%as.matrix(beta.hat) + (phi.hat%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta.hat))) + (theta.hat%*%error[i-(1:q)])
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -217,24 +213,25 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       return(mu.hat)
     }
     
-    ut <-runif(n-m)
+   
+     ut <-runif(n-m)
     mu.hat=mu_residuals(aux$par,y,X)    
     aux$mu <- mu.hat
     p_berg <- d_berg <- NULL
+    
     for (i in (m+1):n) {
       d_berg[i] <-dberg(y[i],mu.hat[i],aux$par[length(aux$par)])
       p_berg[i] <- pberg(y[i]-1,mu.hat[i],aux$par[length(aux$par)])
     }
     aux$resid <- qnorm( p_berg[(m+1):n] + ut*d_berg[(m+1):n])
-} #Final of ARMA Model
-  
+  } #Final of ARMA Model
   
   ##### AR model 
   if(phi != 0 && theta == 0){
     print("AR model")
     
     #Necessary quantities for the CLL function, score and jacobian functions
-    par<- c(alpha,beta,phi,nu)  
+    par<- c(beta,phi,nu)  
     n=length(y)
     p=length(phi);q=0
     m= max(p,q)
@@ -253,14 +250,13 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       eta=error=rep(0,length(y))
       m=max(p,q)
       
-      alpha <- par[1]
-      beta<- par[2:(ncol(X)+1)]
-      phi<-par[(ncol(X)+2):(ncol(X)+p+1)]
+      beta<- par[1:(ncol(X))]
+      phi<-par[(ncol(X)+1):(ncol(X)+p)]
       nu<-par[length(par)]
       #Creating the mu_t; obs: here, I put an threshold when ynew = 0 
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) 
+        eta[i] <- X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) 
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -290,15 +286,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       eta=error=rep(0,length(y))
       m=max(p,q)
       
-      alpha <- par[1]
-      beta<- par[2:(ncol(X)+1)]
-      phi<-par[(ncol(X)+2):(ncol(X)+p+1)]
+      beta<- par[1:(ncol(X))]
+      phi<-par[(ncol(X)+1):(ncol(X)+p)]
       nu<-par[length(par)]
       delta <- (as.numeric(y==0))
       
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) 
+        eta[i] <- X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) 
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -326,31 +321,31 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       
       
       ###Initializing the derivates in zero
-      deta.dalpha <- matrix(0, ncol=1,nrow=n)
+      #deta.dalpha <- matrix(0, ncol=1,nrow=n)
       deta.dbeta <- matrix(0, ncol=length(beta),nrow=n)
       deta.dphi <- matrix(0, ncol=p,nrow=n)
       
       for(i in (m+1):n)
       {
-        deta.dalpha[i,] <- 1 
+        #deta.dalpha[i,] <- 1 
         deta.dbeta[i,]<- B_aux[(i-m),]
         deta.dphi[i,] <- P_aux[(i-m),] 
       }
       ### Derivate with relation to nu
       deta.dnu <- 2*delta*mu/((1-mu+nu)*(1+mu+nu)) + (1-delta)*( 2*(y-mu-nu)/(mu+nu-1)*(mu+nu+1))
       #Matrices use in the score vector (matrix Q = matrix T of the paper)
-      A <- deta.dalpha[(m+1):n]
+      #A <- deta.dalpha[(m+1):n]
       B <- deta.dbeta[(m+1):n,]
       P <- deta.dphi[(m+1):n,]
       D <- diag(as.numeric(1/g1.(mu[(m+1):n])))
       V <- V_aux[(m+1):n]
       
-      Ua <- t(A)%*%D%*%V
+      #Ua <- t(A)%*%D%*%V
       Ub <- t(B)%*%D%*%V
       Up <- t(P)%*%D%*%V
       Un <- sum(deta.dnu[(m+1): n])
       
-      U<- c(Ua,Ub,Up,Un)   
+      U<- c(Ub,Up,Un)   
       return(U)
     }
     
@@ -359,14 +354,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
     U <- function(par) -cond_U_berg(par,y,X,n,p,q,link="log")
     
     opt <- nlminb(par, ll, gradient = NULL, hessian = NULL, scale = 1, control = list(),
-                  lower = rep(0.0001, ncol(X)+p+q+2), 
-                  upper = c(rep(Inf,ncol(X)+1),rep(1,p+q),Inf))
+                  lower = rep(0.0001, ncol(X)+p+q+1), 
+                  upper = c(rep(Inf,ncol(X)),rep(1,p+q),Inf))
     
     
     aux <- c()
     aux$serie <- y
     aux$par <- opt$par
-    names(aux$par)<-c("alpha",paste("beta",1:ncol(X)),paste("phi",1:p),"nu")
+    names(aux$par)<-c(paste("beta",0:(ncol(X)-1)),paste("phi",1:p),"nu")
     aux$loglik <- opt$objective  
     aux$AIC <- 2*aux$loglik+2*(ncol(X)+p+q+1)
     aux$BIC <- 2*aux$loglik+log(n)*(ncol(X)+p+q+1)
@@ -378,16 +373,15 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       # Creating the link functions
       g1 <- g(link)$fun
       g1.inv <- g(link)$inv
-      alpha.hat <- par[1]
-      beta.hat<- par[2:(ncol(X)+1)]
-      phi.hat<-par[(ncol(X)+2):(ncol(X)+p+1)]
+      beta.hat<- par[1:(ncol(X))]
+      phi.hat<-par[(ncol(X)+1):(ncol(X)+p)]
       nu.hat<-par[length(aux$par)]
       X <- as.matrix(X)
       ynew <- g1.inv(y); 
       eta<-error<-rep(0,length(y))
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (phi%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta))) 
+        eta[i] <- X[i,]%*%as.matrix(beta.hat) + (phi.hat%*%(ynew[i-(1:p)]-X[i-(1:p),]%*%as.matrix(beta.hat)))
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -404,6 +398,7 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       d_berg[i] <-dberg(y[i],mu.hat[i],aux$par[length(aux$par)])
       p_berg[i] <- pberg(y[i]-1,mu.hat[i],aux$par[length(aux$par)])
     }
+    
     aux$resid <- qnorm( p_berg[(m+1):n] + ut*d_berg[(m+1):n])
   } #Final of AR Model
   
@@ -412,7 +407,7 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
     print("MA model")
     
     #Necessary quantities for the CLL function, score and jacobian functions
-    par<- c(alpha,beta,theta,nu)  
+    par<- c(beta,theta,nu)  
     n=length(y)
     p=0;q=length(theta)
     m= max(p,q)
@@ -431,14 +426,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       eta=error=rep(0,length(y))
       m=max(p,q)
       
-      alpha <- par[1]
-      beta<- par[2:(ncol(X)+1)]
-      theta<-par[(ncol(X)+2):(ncol(X)+q+1)]
+      
+      beta<- par[1:(ncol(X))]
+      theta<-par[(ncol(X)+1):(ncol(X)+q)]
       nu<-par[length(par)]
       #Creating the mu_t; obs: here, I put an threshold when ynew = 0 
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta)  + (theta%*%error[i-(1:q)])
+        eta[i] <- X[i,]%*%as.matrix(beta)  + (theta%*%error[i-(1:q)])
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -468,20 +463,20 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       eta=error=rep(0,length(y))
       m=max(p,q)
       
-      alpha <- par[1]
-      beta<- par[2:(ncol(X)+1)]
-      theta<-par[(ncol(X)+2):(ncol(X)+q+1)]
+      beta<- par[1:(ncol(X))]
+      theta<-par[(ncol(X)+1):(ncol(X)+q)]
       nu<-par[length(par)]
       delta <- (as.numeric(y==0))
       
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta) + (theta%*%error[i-(1:q)])
+        eta[i] <- X[i,]%*%as.matrix(beta) + (theta%*%error[i-(1:q)])
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
       }
       mu <- g1.inv(eta)
+      
       
       
       
@@ -491,7 +486,6 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
         for(j in 1:length(beta))
           B_aux[i,j] <- X[i+m,j]
       }
-      
       
       
       T_aux <- matrix(rep(NA,(n-m)*q),ncol=q)
@@ -504,31 +498,32 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       
       
       ###Initializing the derivates in zero
-      deta.dalpha <- matrix(0, ncol=1,nrow=n)
+      #deta.dalpha <- matrix(0, ncol=1,nrow=n)
       deta.dbeta <- matrix(0, ncol=length(beta),nrow=n)
+      deta.dphi <- matrix(0, ncol=p,nrow=n)
       deta.dtheta <- matrix(0, ncol=q,nrow=n)
       
       for(i in (m+1):n)
       {
-        deta.dalpha[i,] <- 1 -  theta%*%deta.dalpha[i-(1:q),]
+        #deta.dalpha[i,] <- 1 -  theta%*%deta.dalpha[i-(1:q),]
         deta.dbeta[i,]<- B_aux[(i-m),] - theta%*%deta.dbeta[i-(1:q),]
         deta.dtheta[i,]<- T_aux[(i-m),] - theta%*%deta.dtheta[i-(1:q),]
       }
       ### Derivate with relation to nu
       deta.dnu <- 2*delta*mu/((1-mu+nu)*(1+mu+nu)) + (1-delta)*( 2*(y-mu-nu)/(mu+nu-1)*(mu+nu+1))
       #Matrices use in the score vector (matrix Q = matrix T of the paper)
-      A <- deta.dalpha[(m+1):n]
+      #A <- deta.dalpha[(m+1):n]
       B <- deta.dbeta[(m+1):n,]
       Q <- deta.dtheta[(m+1):n,]
       D <- diag(as.numeric(1/g1.(mu[(m+1):n])))
       V <- V_aux[(m+1):n]
       
-      Ua <- t(A)%*%D%*%V
+      #Ua <- t(A)%*%D%*%V
       Ub <- t(B)%*%D%*%V
       Ut <- t(Q)%*%D%*%V
       Un <- sum(deta.dnu[(m+1): n])
       
-      U<- c(Ua,Ub,Ut,Un)   
+      U<- c(Ub,Ut,Un)   
       return(U)
     }
     
@@ -537,14 +532,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
     U <- function(par) -cond_U_berg(par,y,X,n,p,q,link="log")
     
     opt <- nlminb(par, ll, gradient = NULL, hessian = NULL, scale = 1, control = list(),
-                  lower = rep(0.0001, ncol(X)+p+q+2), 
-                  upper = c(rep(Inf,ncol(X)+1),rep(1,p+q),Inf))
+                  lower = rep(0.0001, ncol(X)+p+q+1), 
+                  upper = c(rep(Inf,ncol(X)),rep(1,p+q),Inf))
     
     
     aux <- c()
     aux$serie <- y
     aux$par <- opt$par
-    names(aux$par)<-c("alpha",rep("beta",ncol(X)),rep("theta",q),"nu")
+    names(aux$par)<-c(paste("beta",0:(ncol(X)-1)),paste("theta",1:q),"nu")
     aux$loglik <- opt$objective  
     aux$AIC <- 2*aux$loglik+2*(ncol(X)+p+q+1)
     aux$BIC <- 2*aux$loglik+log(n)*(ncol(X)+p+q+1)
@@ -556,16 +551,15 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
       # Creating the link functions
       g1 <- g(link)$fun
       g1.inv <- g(link)$inv
-      alpha.hat <- par[1]
-      beta.hat<- par[2:(ncol(X)+1)]
-      theta<-par[(ncol(X)+2):(ncol(X)+q+1)]
+      beta.hat<- par[1:(ncol(X))]
+      theta.hat<-par[(ncol(X)+1):(ncol(X)+q)]
       nu.hat<-par[length(aux$par)]
       X <- as.matrix(X)
       ynew <- g1.inv(y); 
       eta<-error<-rep(0,length(y))
       for(i in (m+1):n)
       {
-        eta[i] <- alpha+X[i,]%*%as.matrix(beta)  + (theta%*%error[i-(1:q)])
+        eta[i] <- X[i,]%*%as.matrix(beta.hat)  + (theta.hat%*%error[i-(1:q)])
         if(y[i]!=0){ynew[i] <- g1(y[i])
         } else {ynew[i] <- g1(const)}
         error[i]<- ynew[i]-eta[i]   
@@ -585,9 +579,6 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
     aux$resid <- qnorm( p_berg[(m+1):n] + ut*d_berg[(m+1):n])
   } #Final of MA Model
   
-  
-  
-  
   return(aux)  
   
   
@@ -596,24 +587,14 @@ bergARMA<- function(alpha,beta,phi,theta,nu, y, X, link = "log"){
 
 
 #Tests
-alpha=0.1;beta=c(0.1);phi=c(0);theta=c(0.1,0.2); nu=2
+alpha=0.5;beta=c(0.0);phi=c(0.1);theta=c(0.2,0.25); nu=5
 n=500
 y<- rbergARMA(n,alpha,beta,phi,theta,nu)
 #y;acf2(y)
 X=cbind(sin(2*pi*(1:(n+100))/12))[101:(100+n)]
-
-fitt=bergARMA(alpha,beta,phi,theta,nu,y,X,link="log")
+X_new=cbind(1,X)
+#X_new=cbind(rep(1,n))
+fitt=bergARMA(c(alpha,beta),phi,theta,nu,y,X_new,link="log")
 fitt$par
 fitt$AIC
-require(astsa)
-require(car)
-acf2(fitt$resid)
-qqPlot(fitt$resid)
-shapiro.test(fitt$resid)
-
-#Usual regression model
-ufit=glm.bg(y~X)
-ufit$coefficients
-acf2(ufit$resid)
-qqPlot(ufit$resid)
-shapiro.test(ufit$resid)
+table(fitt$par[length(fitt$par)]>abs(fitt$mu-1))
